eb168c063fc1220ab4bf2b6c14ba4edc8722fb31
Robert-Jung <rjinjung@gmail.com>
2017-09-05 21:50:12 -0700

    Search for key phrase.

--- crawler.js
+++ crawler.js
@@ -2,7 +2,7 @@ const request = require('request')
 const cheerio = require('cheerio')
 const URL = require('url-parse')
 
-const pageToVisit = 'http://www.homedepot.com/'
+const pageToVisit = 'http://www.homedepot.com/b/Tools-Power-Tools-Power-Tool-Combo-Kits/DEWALT/Cordless/2/N-5yc1vZc2ecZ4j2Z1z0w4czZ1z140i3'
 console.log('Visiting page ' + pageToVisit)
 
 request(pageToVisit, (err, res, body) => {
@@ -12,6 +12,17 @@ request(pageToVisit, (err, res, body) => {
   console.log('Status Code: ' + res.statusCode)
   if (res.statusCode === 200) {
     const $ = cheerio.load(body)
+    searchForWord($, 'Add to Cart to See Price')
     console.log('Page title: ' + $('title').text())
   }
 })
+
+function searchForWord($, word) {
+  const bodyText = $('html > body').text()
+  if (bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+    console.log('true')
+  }
+  else {
+    console.log('false')
+  }
+}
